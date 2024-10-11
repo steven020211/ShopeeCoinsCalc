568a59f5f1a06b40dd3ade890e1e31ac60c4e261
Steven <steven.p@telexper.com>
2024-10-11 18:03:41 +0800

    分類所有的蝦幣獲取方式 加入折線圖

--- ShopeeCoinsCalc/Models/ResultViewModel.cs
+++ ShopeeCoinsCalc/Models/ResultViewModel.cs
@@ -1,16 +1,77 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace ShopeeCoinsCalc.Models;
 
 public class ResultViewModel
 {
 	public bool ShowResult { get; set; } = false;
+	public bool DataError { get; set; } = false;
 
 	public string InputText { get; set; } = string.Empty;
 	public int RetrivedCount { get; set; } = 0;
 	public decimal TotalAmount = 0;
 	public List<IGrouping<decimal, decimal>> SummaryData = new List<IGrouping<decimal, decimal>>();
+
+	public List<IGrouping<string, CoinInfo>> CoinInfos = null!;
+
+    public class CoinInfo
+	{
+		public required string Title;
+		public required string Description;
+		public DateTime Time;
+		public decimal Amount;
+	}
+
+	public void CountNew()
+	{
+        ShowResult = true;
+		
+
+		bool counting()
+		{
+            var list = InputText.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+
+			if (list.Count < 4) return false;
+
+			List<CoinInfo> all = new List<CoinInfo>();
+
+			for(int i = 0;i <list.Count; i+=4)
+			{
+				//data end
+				if (list.Count < i + 4) break;
+				var info = new CoinInfo()
+				{
+					Title = list[i],
+					Description = list[i + 1]
+				};
+
+				if (!DateTime.TryParse(list[i + 2], out info.Time)) return false;
+                if (!decimal.TryParse(list[i + 3], out info.Amount)) return false;
+
+				all.Add(info);
+            }
+
+			CoinInfos = all.GroupBy(x => x.Title).ToList();
+
+			//直播 (相容舊版)
+			var live_group = CoinInfos.FirstOrDefault(g => g.Key == "直播蝦幣");
+
+            TotalAmount = live_group?.Sum(x => x.Amount) ?? 0;
+			RetrivedCount = live_group?.Count() ?? 0;
+
+			SummaryData = live_group?
+				.Select(x => x.Amount)
+				.GroupBy(v => v)
+				.OrderByDescending(v=>v.Key)
+				.ToList() ?? new List<IGrouping<decimal, decimal>>();
+
+			return true;
+        }
+
+		DataError = !counting();
+    }
 
 	public void Count()
 	{
